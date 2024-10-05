95bf653f25065732c463e1698cad01110c8c5add
petraroso <proso00@fesb.hr>
2024-10-05 15:22:45 +0200

    zod installed on server side

--- server/routers/index.ts
+++ server/routers/index.ts
@@ -1,16 +1,12 @@
 import { t } from "../trpc";
+import { z } from "zod";
 
 export const appRouter = t.router({
   sayHi: t.procedure.query(() => {
     return "Hello";
   }),
-  logToServer: t.procedure
-    .input((v) => {
-      if (typeof v === "string") return v;
-      throw new Error("Invalid input");
-    })
-    .mutation((req) => {
-      console.log(req.input);
-      return true;
-    }),
+  logToServer: t.procedure.input(z.string()).mutation((req) => {
+    console.log(req.input);
+    return true;
+  }),
 });
